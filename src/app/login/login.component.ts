1b415c067d7461b6310420c3d887611c0f826b09
Alen Alduk <youstylez@gmail.com>
2023-09-28 13:00:45 +0200

    working on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  isEmailFocused: boolean = false;
+  isPwdFocused: boolean = false;
 
+  onFocusEmail() {
+    this.isEmailFocused = true;
+  }
+
+  onFocusPwd(){
+    this.isPwdFocused = true;
+  }
+
+  onBlurMail(){
+    this.isEmailFocused = false;
+  }
+  onBlurPwd() {
+    this.isPwdFocused = false;
+  }
 }
