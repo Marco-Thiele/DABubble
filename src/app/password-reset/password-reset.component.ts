a38e4effab1eb79dc9de34804148a9d09eb49041
Alen Alduk <youstylez@gmail.com>
2023-10-02 16:27:40 +0200

    working on password reset

--- src/app/password-reset/password-reset.component.ts
+++ src/app/password-reset/password-reset.component.ts
@@ -1,22 +1,43 @@
 import { Component } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { inject } from '@angular/core';
+import { getAuth, sendPasswordResetEmail } from '@angular/fire/auth';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
-  styleUrls: ['./password-reset.component.scss']
+  styleUrls: ['./password-reset.component.scss'],
 })
 export class PasswordResetComponent {
+  firestore: Firestore = inject(Firestore);
+  auth = getAuth();
+  email: string = '';
   isEmailFocused: boolean = false;
- 
+
+  constructor(private UserService: UserService, private _router: Router) {
+    this.auth.languageCode = 'de';
+  }
 
   onFocusEmail() {
     this.isEmailFocused = true;
   }
 
-  onBlurMail(){
+  onBlurMail() {
     this.isEmailFocused = false;
   }
- 
-  
-  recoverUser(){}
+
+  recoverUser() {
+    const auth = getAuth();
+    sendPasswordResetEmail(auth, this.email)
+      .then(() => {
+        console.log('password reset sent!');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
 }
