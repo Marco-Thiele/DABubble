5432f728f742b848d73e4210575273e21861c8cd
Alen Alduk <youstylez@gmail.com>
2023-10-02 14:19:42 +0200

    added login and google login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-
+import { inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInAnonymously,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private UserService: UserService, private _router: Router) {
+    this.auth.languageCode = 'de';
+  }
+
+  firestore: Firestore = inject(Firestore);
+  auth = getAuth();
+  provider = new GoogleAuthProvider();
+  email: string = '';
+  password: string = '';
   isEmailFocused: boolean = false;
   isPwdFocused: boolean = false;
 
@@ -24,5 +43,55 @@ export class LoginComponent {
     this.isPwdFocused = false;
   }
 
-  loginUser() {}
+  loginUser() {
+    signInWithEmailAndPassword(this.auth, this.email, this.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        this._router.navigateByUrl('/index');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
+  guestLogin() {
+    signInAnonymously(this.auth)
+      .then(() => {
+        this.UserService.name = 'Guest';
+        this.UserService.photoURL = 'person.svg';
+        this._router.navigateByUrl('/index');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ...
+      });
+  }
+
+  googleLogin() {
+    signInWithPopup(this.auth, this.provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential != null) {
+          const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+          // IdP data available using getAdditionalUserInfo(result)
+          this._router.navigateByUrl('/index');
+        }
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
 }
