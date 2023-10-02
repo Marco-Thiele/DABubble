8641d87dbd3a707667bb1db16221582033ce5a37
Juan de Santos <juanj.desantos@gmail.com>
2023-10-02 16:09:13 +0200

    main chat + components

---
+++ src/app/channel-edit/channel-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-edit',
+  templateUrl: './channel-edit.component.html',
+  styleUrls: ['./channel-edit.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ChannelEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ChannelEditComponent } from '../channel-edit/channel-edit.component';
 
 @Component({
   selector: 'app-main-chat',
@@ -6,18 +8,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-chat.component.scss'],
 })
 export class MainChatComponent implements OnInit {
-  constructor() {}
+  @ViewChild('imagePreviewCont') imagePreviewCont: ElementRef | undefined;
+  @ViewChild('imagePreview') imagePreview: ElementRef | undefined;
+
+  showIconCatalog = false;
+  name = 'Angular';
+  message = '';
+  showEmojiPicker = false;
+
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  /**
+   * Shows the edit Channel component
+   */
+  editChannel() {
+    this.dialog.open(ChannelEditComponent, {});
+  }
 
   showMembers() {}
 
   addMembers() {}
 
-  onTextareaInput(event: Event) {
+  /**
+   * Regulaces the height of the textarea
+   */
+  onTextareaInput(event: any): void {
     const target = event.target as HTMLTextAreaElement;
     target.style.height = '40px';
     target.style.height = target.scrollHeight + 'px';
-    target.style.paddingBottom = '10px';
+  }
+
+  /**
+   * Charges an image and shows it in the preview
+   */
+  onFileSelected(event: any): void {
+    const selectedFile = event.target.files[0];
+    const previewCont = this.imagePreviewCont?.nativeElement;
+    const previewImg = this.imagePreview?.nativeElement;
+
+    if (selectedFile && previewCont && previewImg) {
+      previewCont.style.display = 'flex';
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target && e.target.result) {
+          previewImg.src = e.target.result as string;
+        }
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  }
+
+  /**
+   * Show the emoji picker
+   */
+  toggleEmojiPicker() {
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
+
+  /**
+   * Add an emoji to the message
+   */
+  addEmoji(event: any) {
+    const { message } = this;
+    const text = `${message}${event.emoji.native}`;
+    this.message = text;
+  }
+
+  /**
+   * Hide the emoji picker when the textarea is focused
+   */
+  onFocus() {
+    this.showEmojiPicker = false;
   }
 }
