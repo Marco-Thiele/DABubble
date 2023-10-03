4b8db62a7bdd3484e2d09d0bf0c78eeeb8d423d9
Juan de Santos <juanj.desantos@gmail.com>
2023-10-03 11:27:03 +0200

    add members dialog, icons and some changes

--- src/app/channel-members/channel-members.component.ts
+++ src/app/channel-members/channel-members.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { AddChannelMembersComponent } from '../add-channel-members/add-channel-members.component';
 
 @Component({
   selector: 'app-channel-members',
@@ -7,7 +9,22 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class ChannelMembersComponent implements OnInit {
-  constructor() {}
+  constructor(
+    public dialogRef: MatDialogRef<ChannelMembersComponent>,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {}
+
+  /**
+   * Closes the dialog for members of the channel.
+   */
+  closeMembers() {
+    this.dialogRef.close();
+  }
+
+  addMember() {
+    this.dialog.open(AddChannelMembersComponent, {});
+    this.dialogRef.close();
+  }
 }
