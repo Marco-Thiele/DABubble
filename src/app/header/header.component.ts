6b0353be71e22258cc9348647bb81c1bebe3a4a7
Marco-Thiele <125655717+Marco-Thiele@users.noreply.github.com>
2023-10-09 16:31:00 +0200

    firestore impement

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserService } from '../user.service';
 import { ProfilComponent } from '../profil/profil.component';
 import { DialogService } from '../dialog.service';
 import { Router } from '@angular/router';
+import { Firestore, doc, getDoc, addDoc, collection, collectionData, onSnapshot, updateDoc, deleteDoc} from '@angular/fire/firestore';
 
 
 @Component({
@@ -13,14 +14,33 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   show: boolean = false;
-  profilName:string;
+  profilName:string| any;
   profilImg:any;
+  firestore: Firestore = inject(Firestore);
 
   constructor(public UserService: UserService, private dialogService: DialogService, private _router: Router) { 
+   setInterval(() => {
     this.profilName = UserService.getName()
     this.profilImg = UserService.getPhoto()
+   },300) 
     console.log(this.profilImg)
+     onSnapshot(this.getChannelsCollection(), (list) => {
+              list.forEach((element) => {
+                console.log(element.data())
+              })
+  });
   }
+
+
+  getChannelsCollection(){
+        return collection(this.firestore, 'channels');
+  }
+
+
+  getPrivateMessagesCollection(){
+    return collection(this.firestore, 'private-messages');
+}
+
   
   showInfo() {
     this.show = true;
