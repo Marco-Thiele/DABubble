3e9322f485541a1c7bfa272b8608d0299a9d2e05
Alen Alduk <youstylez@gmail.com>
2023-10-09 17:33:19 +0200

    updated user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,61 +1,52 @@
 import { Injectable } from '@angular/core';
 import { inject } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
-import { getAuth, onAuthStateChanged } from '@angular/fire/auth';
+import { getAuth, onAuthStateChanged, User } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  name = 'Guest';
-  photoURL: string = '../../assets/img/avatars/person.svg';
-  email: string = 'guest@guest.de';
+  user: User | null = null;
+  name: string = this.getName();
+  photoURL: string = this.getPhoto();
+  email: string = this.getMail();
+  uid: string = this.getId();
   currentUser: any;
-  uid: string = '';
   firestore: Firestore = inject(Firestore);
   auth = getAuth();
 
-  constructor() {}
+  constructor() {
+    this.getUserData();
+  }
 
   getName() {
-    return this.name;
+    return this.user ? this.user.displayName || 'Guest' : 'Guest';
   }
 
   getPhoto() {
-    return this.photoURL;
+    return this.user
+      ? this.user.photoURL || '../../assets/img/avatars/person.svg'
+      : '../../assets/img/avatars/person.svg';
   }
 
   getMail() {
-    return this.email;
+    return this.user ? this.user.email || 'guest@mail.de' : 'guest@mail.de';
+  }
+
+  getId() {
+    return this.user ? this.user.uid || '' : '';
   }
 
   getUserData() {
     onAuthStateChanged(this.auth, (user) => {
       if (user) {
-        if (user.email && user.photoURL && user.displayName && user.uid) {
-          this.email = user.email;
-          this.name = user.displayName;
-          this.photoURL = user.photoURL;
-          this.uid = user.uid;
-          console.log('initiated service');
-        }
+        // User is signed in
+        this.user = user;
       } else {
         console.log('signed out');
+        this.user = null;
       }
-    });
-  }
-
-  getUserName() {
-    onAuthStateChanged(this.auth, (user) => {
-      if (user) {
-        if (user.displayName) {
-          return user.displayName;
-        }
-      } else {
-        console.log('user signed out');
-        return 'Guest';
-      }
-      return undefined;
     });
   }
 }
