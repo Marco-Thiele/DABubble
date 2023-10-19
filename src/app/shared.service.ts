800396fbe3f016fb80c817b0b6a8636ada9e15db
Marco-Thiele <125655717+Marco-Thiele@users.noreply.github.com>
2023-10-19 20:35:40 +0200

    merge

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -24,10 +24,8 @@ import {
 export class SharedService implements OnInit {
   private channels: any[] = [];
   private isEditChannelOpen = false;
-  public members: any[] = [];
+  private members: any[] = [];
   private membersData = new BehaviorSubject<any[]>([]);
-  private channelsData = new BehaviorSubject<any[]>([]);
-  private userData: any;
 
   currentMembers = this.membersData.asObservable();
   firestore: Firestore = inject(Firestore);
@@ -78,7 +76,7 @@ export class SharedService implements OnInit {
     }
   }
 
-  async privateMsgList(selectedMember: any) {
+  async subChatList(selectedMember: any) {
     console.log('subChatList');
     const querySnapshot = await getDocs(
       query(
@@ -233,7 +231,7 @@ export class SharedService implements OnInit {
       this.membersListArray = [];
       list.forEach((element) => {
         this.membersListArray.push(
-          this.setMemberObject(element.data(), element.id)
+          this.setChannelObject(element.data(), element.id)
         );
       });
     });
@@ -242,24 +240,6 @@ export class SharedService implements OnInit {
   //return the members from firestore
   getMembersFromFS() {
     return collection(this.firestore, 'members');
-  }
-
-  async getMembersFS() {
-    const querySnapshot = await getDocs(collection(this.firestore, 'members'));
-    this.members = querySnapshot.docs.map((doc) =>
-      this.setMemberObject(doc.data(), doc.id)
-    );
-    this.membersData.next(this.members);
-    return this.members;
-  }
-
-  async getChannelsFS() {
-    const querySnapshot = await getDocs(collection(this.firestore, 'channels'));
-    this.channels = querySnapshot.docs.map((doc) =>
-      this.setMemberObject(doc.data(), doc.id)
-    );
-    this.channelsData.next(this.channels);
-    return this.channels;
   }
 
   //return the channels from firestore
@@ -279,40 +259,19 @@ export class SharedService implements OnInit {
     };
   }
 
-  //sets the members object from firestore
-  setMemberObject(obj: any, id: string) {
-    return {
-      id: id,
-      chat: obj.chat || [],
-      imgProfil: obj.imgProfil || '',
-      member: obj.member || [],
-      name: obj.name || '',
-      type: obj.type || '',
-      channels: obj.channels || [],
-    };
-  }
-
   //gets the single document reference from firestore
   getsingleDocRef(colId: string, docId: string) {
     return doc(collection(this.firestore, colId), docId);
-  }
-
-  getUserData() {
-    return this.userData;
-  }
-
-  setUserData(userData: any) {
-    this.userData = userData;
   }
 
   /**
    * updates the members in channel
    * @param members  the members to update
    */
-  // updateMembers(members: any[]) {
-  //   this.membersData.next(members);
-  //   // this.saveChannelToLocalStorage();
-  // }
+  updateMembers(members: any[]) {
+    this.membersData.next(members);
+    this.saveChannelToLocalStorage();
+  }
 
   /**
    * Gets the id of the current user.
@@ -384,18 +343,18 @@ export class SharedService implements OnInit {
    * Adds a channel to the list of channels.
    * @param channel the channel to add
    */
-  // addChannel(channel: any) {
-  //   this.channels.push(channel);
-  //   this.saveChannelToLocalStorage();
-  // }
+  addChannel(channel: any) {
+    this.channels.push(channel);
+    this.saveChannelToLocalStorage();
+  }
 
   /**
    * Gets the list of channels.
    * @returns the list of channels
    */
-  // getChannels() {
-  //   return this.channels;
-  // }
+  getChannels() {
+    return this.channels;
+  }
 
   getChannelsFromLS(): any[] {
     const storedChannels = localStorage.getItem('channels');
@@ -407,33 +366,33 @@ export class SharedService implements OnInit {
    * @param id the id of the channel
    * @returns the channel
    */
-  // updateChannel(updatedChannel: any) {
-  //   const index = this.channels.findIndex(
-  //     (channel) => channel.id === updatedChannel.id
-  //   );
-  //   if (index !== -1) {
-  //     this.channels[index] = updatedChannel;
-  //     this.saveChannelToLocalStorage();
-  //   }
-  // }
+  updateChannel(updatedChannel: any) {
+    const index = this.channels.findIndex(
+      (channel) => channel.id === updatedChannel.id
+    );
+    if (index !== -1) {
+      this.channels[index] = updatedChannel;
+      this.saveChannelToLocalStorage();
+    }
+  }
 
   /**
    * Saves the channel to local storage.
    * @param id the id of the channel
    * @returns the channel
    */
-  // private saveChannelToLocalStorage() {
-  //   localStorage.setItem('channels', JSON.stringify(this.channels));
-  // }
+  private saveChannelToLocalStorage() {
+    localStorage.setItem('channels', JSON.stringify(this.channels));
+  }
 
   /**
    * Adds a member to the list of members.
    * @param member the member to add
    */
-  // addMember(member: any) {
-  //   this.members.push(member);
-  //   this.saveMembersToLocalStorage();
-  // }
+  addMember(member: any) {
+    this.members.push(member);
+    this.saveMembersToLocalStorage();
+  }
 
   /**
    * Gets the list of members.
@@ -449,109 +408,109 @@ export class SharedService implements OnInit {
    * @param id the id of the member
    * @returns the member
    */
-  // saveMembersToLocalStorage() {
-  //   localStorage.setItem('members', JSON.stringify(this.members));
-  // }
+  saveMembersToLocalStorage() {
+    localStorage.setItem('members', JSON.stringify(this.members));
+  }
 
   /**
    * updates the member to local storage.
    * @param id the id of the member
    * @returns the member
    */
-  // updateMember(updatedMember: any) {
-  //   const index = this.members.findIndex(
-  //     (member) => member.id === updatedMember.id
-  //   );
-  //   if (index !== -1) {
-  //     this.members[index] = updatedMember;
-  //     this.saveMembersToLocalStorage();
-  //   }
-  // }
+  updateMember(updatedMember: any) {
+    const index = this.members.findIndex(
+      (member) => member.id === updatedMember.id
+    );
+    if (index !== -1) {
+      this.members[index] = updatedMember;
+      this.saveMembersToLocalStorage();
+    }
+  }
 
   /**
    * saves the message of channels to local storage.
    * @returns the list of private messages
    */
-  // saveMessageToLocalStorage(channelId: string, message: any) {
-  //   const storedChannels = localStorage.getItem('channels');
-  //   const channels = storedChannels ? JSON.parse(storedChannels) : [];
+  saveMessageToLocalStorage(channelId: string, message: any) {
+    const storedChannels = localStorage.getItem('channels');
+    const channels = storedChannels ? JSON.parse(storedChannels) : [];
 
-  //   const channel = channels.find((c: any) => c.id === channelId);
+    const channel = channels.find((c: any) => c.id === channelId);
 
-  //   if (channel) {
-  //     channel.chat.push(message);
-  //   }
+    if (channel) {
+      channel.chat.push(message);
+    }
 
-  //   localStorage.setItem('channels', JSON.stringify(channels));
-  // }
+    localStorage.setItem('channels', JSON.stringify(channels));
+  }
 
   /**
    * saves the message of private messages to local storage.
    * @returns the list of private messages
    */
-  // savePrivateMessageToLocalStorage(memberId: string, message: any) {
-  //   const storedPrivateMsg = localStorage.getItem('privateMessages');
-  //   let privateMessages = storedPrivateMsg ? JSON.parse(storedPrivateMsg) : {};
+  savePrivateMessageToLocalStorage(memberId: string, message: any) {
+    const storedPrivateMsg = localStorage.getItem('privateMessages');
+    let privateMessages = storedPrivateMsg ? JSON.parse(storedPrivateMsg) : {};
 
-  //   if (!privateMessages[memberId]) {
-  //     privateMessages[memberId] = {
-  //       id: memberId,
-  //       chat: [],
-  //     };
-  //   }
+    if (!privateMessages[memberId]) {
+      privateMessages[memberId] = {
+        id: memberId,
+        chat: [],
+      };
+    }
 
-  //   privateMessages[memberId].chat.push(message);
-  //   localStorage.setItem('privateMessages', JSON.stringify(privateMessages));
-  // }
+    privateMessages[memberId].chat.push(message);
+    localStorage.setItem('privateMessages', JSON.stringify(privateMessages));
+  }
 
   /**
    * gets the message of channels from local storage.
    * @returns the list of channels messages
    */
-  // getChannelsIds(): {
-  //   [channelId: string]: { chat: any[] };
-  // } {
-  //   const channelMessages: {
-  //     [channelId: string]: { chat: any[] };
-  //   } = {};
+  getChannelsIds(): {
+    [channelId: string]: { chat: any[] };
+  } {
+    const channelMessages: {
+      [channelId: string]: { chat: any[] };
+    } = {};
 
-  //   const storedChannels = localStorage.getItem('channels');
-  //   const channels = storedChannels ? JSON.parse(storedChannels) : [];
-  //   channels.forEach((channel: any) => {
-  //     const channelId = channel.id;
-  //     channelMessages[channelId] = {
-  //       chat: this.getMessagesForChannel(channelId),
-  //     };
-  //   });
+    const storedChannels = localStorage.getItem('channels');
+    const channels = storedChannels ? JSON.parse(storedChannels) : [];
+    channels.forEach((channel: any) => {
+      const channelId = channel.id;
+      channelMessages[channelId] = {
+        chat: this.getMessagesForChannel(channelId),
+      };
+    });
 
-  //   return channelMessages;
-  // }
+    return channelMessages;
+  }
 
   /**
    * return the message for the channels
    * @returns the list of private messages
    */
-  // getMessagesForChannel(channelId: string): any[] {
-  //   const storedChannels = localStorage.getItem('channels');
-  //   const channels = storedChannels ? JSON.parse(storedChannels) : [];
+  getMessagesForChannel(channelId: string): any[] {
+    const storedChannels = localStorage.getItem('channels');
+    const channels = storedChannels ? JSON.parse(storedChannels) : [];
 
-  //   const channel = channels.find((c: any) => c.id === channelId);
+    const channel = channels.find((c: any) => c.id === channelId);
 
-  //   return channel ? channel.chat : [];
-  // }
+    return channel ? channel.chat : [];
+  }
 
   /**
    * return the message for the private messages
    * @returns the list of private messages
    */
-  //   returnPrivateChats(memberId: string) {
-  //     const storedPrivateMsg = localStorage.getItem('privateMessages');
-  //     if (storedPrivateMsg) {
-  //       const privateMessages = JSON.parse(storedPrivateMsg);
-  //       if (privateMessages[memberId]) {
-  //         return privateMessages[memberId] || [];
-  //       }
-  //     }
-  //     return { chat: [] };
-  //   }
+  returnPrivateChats(memberId: string) {
+    const storedPrivateMsg = localStorage.getItem('privateMessages');
+    if (storedPrivateMsg) {
+      const privateMessages = JSON.parse(storedPrivateMsg);
+      if (privateMessages[memberId]) {
+        return privateMessages[memberId] || [];
+      }
+    }
+    return { chat: [] };
+  }
 }
