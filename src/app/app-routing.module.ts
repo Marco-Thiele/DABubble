b095687997d96fd92482c5dcbe93531390e56651
Alen Alduk <youstylez@gmail.com>
2023-11-05 19:09:02 +0100

    css changes on account creation and route changes to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { ImpressumComponent } from './login-components/impressum/impressum.compo
 import { DataPolicyComponent } from './login-components/data-policy/data-policy.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: CreateAccountComponent },
   { path: 'pick-avatar', component: PickAvatarComponent },
